3b22fb52f0f60caaf7a408fff0cb95e99cab030d
Mareh23 <marehlof@gmail.com>
2023-01-11 19:03:25 -0300

    db

--- src/main/java/com/miportafolio/mariaz/Controller/CHys.java
+++ src/main/java/com/miportafolio/mariaz/Controller/CHys.java
@@ -13,7 +13,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-//@CrossOrigin(origins = "http://localhost:4200")
 
 @RequestMapping("/skill")
 public class CHys {
