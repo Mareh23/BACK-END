b450e5acf3842d071e654f0a98cc7518e7ba0e39
Mareh23 <marehlof@gmail.com>
2023-01-10 22:12:44 -0300

    Skills

---
+++ src/main/java/com/miportafolio/mariaz/Dto/dtoHys.java
@@ -0,0 +1,39 @@
+package com.miportafolio.mariaz.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class dtoHys {
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private int porcentaje;
+
+    //Constructor
+
+    public dtoHys() {
+    }
+
+    public dtoHys(String nombre, int porcentaje) {
+        this.nombre = nombre;
+        this.porcentaje = porcentaje;
+    }
+    
+    //Getters & Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(int porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+    
+}
