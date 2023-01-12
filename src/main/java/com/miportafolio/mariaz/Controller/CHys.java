a0c53bbcb1d095d2c56921ed7cca2deca9678b8b
Mareh23 <marehlof@gmail.com>
2023-01-11 22:04:10 -0300

    errores cors

--- src/main/java/com/miportafolio/mariaz/Controller/CHys.java
+++ src/main/java/com/miportafolio/mariaz/Controller/CHys.java
@@ -13,6 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,8 +21,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@CrossOrigin(origins = "https://front-end-mariz.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
