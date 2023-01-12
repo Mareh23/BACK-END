7e3e6602ef9522c0b03f27439a4fd9cbbd6e2a43
Mareh23 <marehlof@gmail.com>
2023-01-11 23:02:04 -0300

    errores cors

--- src/main/java/com/miportafolio/mariaz/Controller/PersonaController.java
+++ src/main/java/com/miportafolio/mariaz/Controller/PersonaController.java
@@ -54,7 +54,7 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         return persona;
     }
-    @GetMapping("personas/traer/perfil")
+    @GetMapping("/personas/traer/perfil")
     public Persona findPersona() {
         return ipersonaService.findPersona((long)1);
     }
