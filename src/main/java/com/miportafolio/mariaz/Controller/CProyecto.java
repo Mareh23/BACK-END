0d0f1d027d381c604458af9ec9aa53844710919a
Mareh23 <marehlof@gmail.com>
2023-01-14 11:01:33 -0300

    errors

--- src/main/java/com/miportafolio/mariaz/Controller/CProyecto.java
+++ src/main/java/com/miportafolio/mariaz/Controller/CProyecto.java
@@ -32,61 +32,56 @@ public class CProyecto {
         List<Proyecto> list = sProyecto.list();
         return new ResponseEntity(list, HttpStatus.OK);
     }
+    
     @GetMapping("/detail/{id}")
-    public ResponseEntity<Proyecto> getById(@PathVariable("id")int id){
-        if(!sProyecto.existsById(id)){
-            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
-        }
-        
+    public ResponseEntity<Proyecto> getById(@PathVariable("id") int id){
+        if(!sProyecto.existsById(id))
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
         Proyecto proyecto = sProyecto.getOne(id).get();
         return new ResponseEntity(proyecto, HttpStatus.OK);
     }
     
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") int id){
-        if(!sProyecto.existsById(id)){
-            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (!sProyecto.existsById(id)) {
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
         }
         sProyecto.delete(id);
-        return new ResponseEntity(new Mensaje("Proyecto eliminado"), HttpStatus.OK);
+        return new ResponseEntity(new Mensaje("producto eliminado"), HttpStatus.OK);
     }
+
     
     @PostMapping("/create")
-    public ResponseEntity<?> create(@RequestBody dtoProyecto dtoproyecto){
-        if(StringUtils.isBlank(dtoproyecto.getNombreP())){
+    public ResponseEntity<?> create(@RequestBody dtoProyecto dtoproyecto){      
+        if(StringUtils.isBlank(dtoproyecto.getNombreP()))
             return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
-        }
-        if(sProyecto.existsByNombreP(dtoproyecto.getNombreP())){
-            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
-        }
+        if(sProyecto.existsByNombreP(dtoproyecto.getNombreP()))
+            return new ResponseEntity(new Mensaje("Esa experiencia existe"), HttpStatus.BAD_REQUEST);
         
-        Proyecto proyecto = new Proyecto(
-                dtoproyecto.getNombreP(), dtoproyecto.getDescripcionP()
-            );
+        Proyecto proyecto = new Proyecto(dtoproyecto.getNombreP(), dtoproyecto.getDescripcionP());
         sProyecto.save(proyecto);
-        return new ResponseEntity(new Mensaje("Proyecto creado"), HttpStatus.OK);
-                
+        
+        return new ResponseEntity(new Mensaje("Proyecto agregado"), HttpStatus.OK);
     }
     
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoProyecto dtoproyecto){
-        if(!sProyecto.existsById(id)){
-            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
-        }
-        if(sProyecto.existsByNombreP(dtoproyecto.getNombreP()) && sProyecto.getByNmbreP(dtoproyecto.getNombreP()).get().getId() != id){
-            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
-        }
-        if(StringUtils.isBlank(dtoproyecto.getNombreP())){
-            return new ResponseEntity(new Mensaje("El campo no puede estar vacio"), HttpStatus.BAD_REQUEST);
-        }
+        //Validamos si existe el ID
+        if(!sProyecto.existsById(id))
+            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
+        //Compara nombre de experiencias
+        if(sProyecto.existsByNombreP(dtoproyecto.getNombreP()) && sProyecto.getByNombreP(dtoproyecto.getNombreP()).get().getId() != id)
+            return new ResponseEntity(new Mensaje("Esa experiencia ya existe"), HttpStatus.BAD_REQUEST);
+        //No puede estar vacio
+        if(StringUtils.isBlank(dtoproyecto.getNombreP()))
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
         
         Proyecto proyecto = sProyecto.getOne(id).get();
-        
         proyecto.setNombreP(dtoproyecto.getNombreP());
-        proyecto.setDescripcionP(dtoproyecto.getDescripcionP());
+        proyecto.setDescripcionP((dtoproyecto.getDescripcionP()));
         
         sProyecto.save(proyecto);
-        
-        return new ResponseEntity(new Mensaje("Proyecto actualizado"), HttpStatus.OK);
+        return new ResponseEntity(new Mensaje("Proyecto actualizada"), HttpStatus.OK);
+             
     }
 }
--- src/main/java/com/miportafolio/mariaz/Service/SProyecto.java
+++ src/main/java/com/miportafolio/mariaz/Service/SProyecto.java
@@ -13,33 +13,33 @@ import javax.transaction.Transactional;
 @Transactional
 public class SProyecto {
     @Autowired
-    RProyecto rProyecto;
-    
-    public List<Proyecto> list(){
-        return rProyecto.findAll();
-    }
-    
-    public Optional<Proyecto> getOne(int id){
-        return rProyecto.findById(id);
-    }
-    
-    public Optional<Proyecto> getByNmbreP(String nombreP){
-        return rProyecto.findByNombreP(nombreP);
-    }
-    
-    public void save(Proyecto proyecto){
-        rProyecto.save(proyecto);
-    }
-    
-    public void delete(int id){
-        rProyecto.deleteById(id);
-    }
-    
-    public boolean existsById(int id){
-        return rProyecto.existsById(id);
-    }
-    
-    public boolean existsByNombreP(String nombreP){
-        return rProyecto.existsByNombreP(nombreP);
-    }
+     RProyecto rProyecto;
+     
+     public List<Proyecto> list(){
+         return rProyecto.findAll();
+     }
+     
+     public Optional<Proyecto> getOne(int id){
+         return rProyecto.findById(id);
+     }
+     
+     public Optional<Proyecto> getByNombreP(String nombreP){
+         return rProyecto.findByNombreP(nombreP);
+     }
+     
+     public void save(Proyecto proyecto){
+         rProyecto.save(proyecto);
+     }
+     
+     public void delete(int id){
+         rProyecto.deleteById(id);
+     }
+     
+     public boolean existsById(int id){
+         return rProyecto.existsById(id);
+     }
+     
+     public boolean existsByNombreP(String nombreP){
+         return rProyecto.existsByNombreP(nombreP);
+     }
 }
