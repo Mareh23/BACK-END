1ac0708e1fc92ce252dacee177564ff65e58ca95
Mareh23 <marehlof@gmail.com>
2023-01-14 14:41:04 -0300

    proyecto

---
+++ src/main/java/com/miportafolio/mariaz/Entity/Proyecto.java
@@ -0,0 +1,52 @@
+package com.miportafolio.mariaz.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Proyecto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombreP;
+    private String descripcionP;
+    
+    //constructores
+
+    public Proyecto() {
+    }
+
+    public Proyecto(String nombreP, String descripcionP) {
+        this.nombreP = nombreP;
+        this.descripcionP = descripcionP;
+    }
+    
+    //getters & setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreP() {
+        return nombreP;
+    }
+
+    public void setNombreP(String nombreP) {
+        this.nombreP = nombreP;
+    }
+
+    public String getDescripcionP() {
+        return descripcionP;
+    }
+
+    public void setDescripcionP(String descripcionP) {
+        this.descripcionP = descripcionP;
+    }
+    
+}
