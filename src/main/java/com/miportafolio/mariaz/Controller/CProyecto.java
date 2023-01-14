ec95af50760efa168f5f228fe900993ba10e41d5
Mareh23 <marehlof@gmail.com>
2023-01-14 20:51:02 -0300

    errors

--- src/main/java/com/miportafolio/mariaz/Controller/CProyecto.java
+++ src/main/java/com/miportafolio/mariaz/Controller/CProyecto.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyect")
-@CrossOrigin(origins = {"https://front-end-mariz.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = "https://front-end-mariz.web.app")
 public class CProyecto {
     @Autowired
     SProyecto sProyecto;
