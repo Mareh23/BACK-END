c59a33d850e1d57f0258c60e433dd354b59944b3
Mareh23 <marehlof@gmail.com>
2023-01-13 21:59:10 -0300

    proyecto

---
+++ src/main/java/com/miportafolio/mariaz/Dto/dtoProyecto.java
@@ -0,0 +1,41 @@
+package com.miportafolio.mariaz.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+
+public class dtoProyecto {
+    @NotBlank
+    private String nombreP;
+    @NotBlank
+    private String descripcionP;
+    
+    //Constructores
+
+    public dtoProyecto() {
+    }
+
+    public dtoProyecto(String nombreP, String descripcionP) {
+        this.nombreP = nombreP;
+        this.descripcionP = descripcionP;
+    }
+    
+    //Getters & Setters
+
+    public String getNombreP() {
+        return nombreP;
+    }
+
+    public void setNombreP(String nombreP) {
+        this.nombreP = nombreP;
+    }
+
+    public String getDescripcionP() {
+        return descripcionP;
+    }
+
+    public void setDescripcionP(String descripcionP) {
+        this.descripcionP = descripcionP;
+    }
+
+    
+}
