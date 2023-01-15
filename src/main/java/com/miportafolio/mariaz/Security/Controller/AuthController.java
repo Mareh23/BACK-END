03df09a1e1c67901c5f024823d267aae605846ce
Mareh23 <marehlof@gmail.com>
2023-01-15 03:01:35 -0300

    cross origin

--- src/main/java/com/miportafolio/mariaz/Security/Controller/AuthController.java
+++ src/main/java/com/miportafolio/mariaz/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://front-end-mariz.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
