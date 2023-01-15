289926291452071822d3b51891b588681d5fce04
Mareh23 <marehlof@gmail.com>
2023-01-15 19:29:35 -0300

    correction

--- src/main/java/com/miportafolio/mariaz/Controller/CHys.java
+++ src/main/java/com/miportafolio/mariaz/Controller/CHys.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/skill")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class CHys {
 
     @Autowired
--- src/main/java/com/miportafolio/mariaz/Controller/PersonaController.java
+++ src/main/java/com/miportafolio/mariaz/Controller/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
