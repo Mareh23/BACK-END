52d53d5e96d2a62babaf40b7fc75904526b66b55
Mareh23 <marehlof@gmail.com>
2023-01-14 23:11:59 -0300

    errors

--- src/main/java/com/miportafolio/mariaz/Controller/CEducacion.java
+++ src/main/java/com/miportafolio/mariaz/Controller/CEducacion.java
@@ -25,8 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-//@CrossOrigin(origins = "https://front-end-mariz.web.app")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://front-end-mariz.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
--- src/main/java/com/miportafolio/mariaz/Controller/CProyecto.java
+++ src/main/java/com/miportafolio/mariaz/Controller/CProyecto.java
@@ -21,8 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyect")
-//@CrossOrigin(origins = "https://front-end-mariz.web.app")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://front-end-mariz.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CProyecto {
     @Autowired
     SProyecto sProyecto;
