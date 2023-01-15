82da23e13881b82d890c9bfa5a19f0477b2c39cc
Mareh23 <marehlof@gmail.com>
2023-01-14 23:25:18 -0300

    errors

--- src/main/java/com/miportafolio/mariaz/Controller/PersonaController.java
+++ src/main/java/com/miportafolio/mariaz/Controller/PersonaController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/personas")
 @CrossOrigin(origins = "https://front-end-mariz.web.app")
-////@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
