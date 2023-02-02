efd7257a9796c693af07a79e986b746cd16c1e7b
Mareh23 <marehlof@gmail.com>
2023-02-01 23:27:37 -0300

    errores para deploy

--- src/main/java/com/miportafolio/mariaz/Security/jwt/JwtTokenFilter.java
+++ src/main/java/com/miportafolio/mariaz/Security/jwt/JwtTokenFilter.java
@@ -16,8 +16,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class JwtTokenFilter extends OncePerRequestFilter {
+
     private final static Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
-    
+
     @Autowired
     JwtProvider jwtProvider;
     @Autowired
@@ -27,22 +28,24 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             String token = getToken(request);
-            if(token != null && jwtProvider.validateToken(token)){
+            if (token != null && jwtProvider.validateToken(token)) {
                 String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
                 UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(nombreUsuario);
                 UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
-        } catch (UsernameNotFoundException e){
-                    logger.error("Falló el método doFilterInternal");
+        } catch (UsernameNotFoundException e) {
+            logger.error("Falló el método doFilterInternal");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "El token no es válido o ha expirado");
         }
-            filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
-    
-    private String getToken(HttpServletRequest request){
+
+    private String getToken(HttpServletRequest request) {
         String header = request.getHeader("Authorization");
-        if (header != null && header.startsWith("Bearer"))
+        if (header != null && header.startsWith("Bearer")) {
             return header.replace("Bearer", "");
+        }
         return null;
     }
 }
